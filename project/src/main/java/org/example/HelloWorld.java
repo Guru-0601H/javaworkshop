af6041a61b42ca4ee54ee9e95f7d435c6f445f98
Guru-0601H <prasathrguru6@gmail.com>
2025-06-24 18:46:22 +0530

    t5t5g

--- project/src/main/java/org/example/HelloWorld.java
+++ project/src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorlhhyyd");
     }
 }
